961aa568247cc52317ae2e701709814c0aed13f6
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-22 11:41:46 +0700

    5. The Basics of Python: 4. Getting To Know Python

---
+++ PythonMasterclass/05.BasicOfPython/HelloWorld/HelloWorld.py
@@ -0,0 +1,20 @@
+print('Hello World')
+
+print(1 + 2)
+print(7 * 6)
+
+print()
+
+print('The End')
+
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+
+print("Hello " + "World")
+
+greeting = "Hello"
+name = "Gekif"
+print(greeting + name)
+
+# If we want a space, we can add that too
+print(greeting + ' ' + name)
