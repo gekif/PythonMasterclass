6a75361a36890c6239f688010cb78e43e368f0de
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-22 13:09:55 +0700

    5. The Basics of Python: 11. More About Variables And Strings

---
+++ PythonMasterclass/05.BasicOfPython/Variables/Variables.py
@@ -0,0 +1,32 @@
+greeting = "Hello"
+_myName = "Gekif"
+# Gekif45
+# Gekif_was_45
+# Greeting
+# print(greeting + ' ' + _myName)
+
+age = 24
+# print(age)
+
+# print(greeting + age)
+
+a = 12
+b = 3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b) # return float
+# print(a // b) # return integer
+# print(a % b)
+
+# for i in range(1, 4):
+#     print(i)
+#
+# for i in range(1, a // b):
+#     print(i)
+
+print(a + b / 3 - 4 * 12)
+print( 8 // 2 * 3)
+print (8 * 3 // 2)
+
+print ( (a + b) // 3)
