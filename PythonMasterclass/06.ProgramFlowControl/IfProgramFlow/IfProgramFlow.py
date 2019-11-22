3edb2b2306940383ba74b328c00b8f4c7795167e
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-22 15:35:39 +0700

    6. Program Flow Control in Python: 5. More Advanced If, ElIf & Else Processing

--- PythonMasterclass/06.ProgramFlowControl/IfProgramFlow/IfProgramFlow.py
+++ PythonMasterclass/06.ProgramFlowControl/IfProgramFlow/IfProgramFlow.py
@@ -1,54 +1,49 @@
-# name = input("Please enter your name: ")
-# age = int(input("How old are you, {0}? " . format(name)))
-# print(age)
+# age = int(input("How old are you? "))
+
+# if (age >= 16) and (age <= 65):
+
+# same result as above
+# if 15 <= age <= 45:
+# if 15 < age < 45:
+#     print("Have a good day at work")
+
+
+# if (age < 16) or (age > 65):
+#     print("Enjoy your free time")
+# else:
+#     print("Have a good day at work")
+
+
+# x = "true"
+# if x:
+#     print("x is true")
+
+
+# x = input("Please enter some text ")
 #
-# if age >= 18:
+# if x:
+#     print("You entered '{}'" . format(x))
+# else:
+#     print("You did not enter anything")
+
+
+# print(not True)
+# print(not False)
+
+
+# age = int(input("How old are you? "))
+# if not(age < 18):
 #     print("You are old enough to vote")
 #     print("Please put an X in the box")
 # else:
 #     print("Please come back in {0} years" . format(18 - age))
 
 
-# print("Please guess a number between 1 and 10: ")
-# guess = int(input())
-#
-# if guess < 5:
-#     print("Please guess higher")
-#     guess = int(input())
-#
-#     if guess == 5:
-#         print("Well done you guessed it")
-#     else:
-#         print("Sorry you have not guessed correctly")
-#
-# elif guess > 5:
-#     print("Please guess lower")
-#     guess = int(input())
-#
-#     if guess == 5:
-#         print("Well done you guessed it")
-#     else:
-#         print("Sorry you have not guessed correctly")
-#
-# else:
-#     print("You got it first time")
+parrot = "Norwegian Blue"
 
+letter = input("Enter a character: ")
 
-print("Please guess a number between 1 and 10: ")
-guess = int(input())
-
-if guess != 5:
-    if guess < 5:
-        print("Please guess higher")
-    else:
-        print("Please guess lower")
-
-    guess = int(input())
-
-    if guess == 5:
-        print("Well done you guessed it")
-    else:
-        print("Sorry you have not guessed correctly")
-
+if letter in parrot:
+    print("Give me an '{}', Bob" . format(letter))
 else:
-    print("You got it first time")
+    print("I don't need that letter")
