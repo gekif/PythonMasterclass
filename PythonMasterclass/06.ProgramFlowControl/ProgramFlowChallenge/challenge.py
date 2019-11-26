c5e5376787565ad357c5565fbab53fba7951cbab
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-11-26 10:26:46 +0700

    6. Program Flow Control in Python: 19. Challenge - Program Flow - Part 2

--- PythonMasterclass/06.ProgramFlowControl/ProgramFlowChallenge/challenge.py
+++ PythonMasterclass/06.ProgramFlowControl/ProgramFlowChallenge/challenge.py
@@ -1,17 +1,23 @@
-ipAddress = input("Please enter an IP Address: ")
+inputPrompt = "Please enter an IP Address. An IP address consists of 4 numbers, " \
+                  "separated from each other with a full stop: "
+
+ipAddress = input(inputPrompt)
+
+if ipAddress[-1] != '.':
+    ipAddress += '.'
 
 segment = 1
 segmentLength = 0
-character = ""
+# character = ""
 
 for character in ipAddress:
     if character == '.':
-        print("Segment {} contains {} charcters" . format(segment, segmentLength))
+        print('Segment {} contains {} characters'. format(segment, segmentLength))
         segment += 1
         segmentLength = 0
     else:
         segmentLength += 1
 
 # Unless the final character in the string was a . then we haven't printed the last segment
-if character != '.':
-    print("Segment {} contains {} characters" . format(segment, segmentLength))
+# if character != '.':
+#     print("Segment {} contains {} characters" . format(segment, segmentLength))
